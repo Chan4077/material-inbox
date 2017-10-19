70d4c1fbc3a568034d0b39d778481f7164db31ed
Edric Chan <edric.chan.1997@gmail.com>
2017-10-19 12:41:43 +0800

    chore: Remove placeholder content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Shared } from './shared';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private breakpointObserver: BreakpointObserver, private shared: Shared){}
+	isRefreshing: boolean = false;
+	@ViewChild('left') sidenav: MatSidenav;
+	get isSidenavOpen() {
+		if (this.sidenav.opened) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	get isMobile() {
+		if (this.breakpointObserver.isMatched('(max-width: 599px)')) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	links: NavLink[] = [
+		{
+			link: "home",
+			title: "Inbox",
+			icon: "inbox"
+		},
+		{
+			link: "snoozed",
+			title: "Snoozed",
+			icon: "access_time"
+		},
+		{
+			link: "done",
+			title: "Done",
+			icon: "check"
+		}
+	];
+	otherLinks: NavLink[] = [
+		{
+			link: "drafts",
+			title: "Drafts",
+			icon: "drafts"
+		},
+		{
+			link: "sent",
+			title: "Sent",
+			icon: "send"
+		},
+		{
+			link: "reminders",
+			title: "Reminders",
+			icon: "alarm"
+		},
+		{
+			link: "bin",
+			title: "Bin",
+			icon: "delete"
+		},
+		{
+			link: "spam",
+			title: "Spam",
+			icon: "warning"
+		}
+	]
+	refresh() {
+		this.isRefreshing = true;
+		setTimeout(()=> {
+			this.isRefreshing = false;
+			let snackBarRef = this.shared.openSnackBarWithRef({msg: "1 new message found!", action: "View message", additionalOpts: {duration: 4000, horizontalPosition: "start"}});
+			snackBarRef.onAction().subscribe(_ => {
+				console.log("Navigating to new message...");
+			})
+		}, 3000)
+	}
+}
+
+interface NavLink {
+	link: string;
+	title: string;
+	icon?: string;
 }
